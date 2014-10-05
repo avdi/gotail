46ae30fec324a3f39b1a6bb4caec1347e43084c9
Avdi Grimm <avdi@avdi.org>
2014-10-05 16:05:14 -0400

    initial

---
+++ gotail.go
@@ -0,0 +1,85 @@
+package main
+
+import "os"
+import "log"
+import "fmt"
+import "bytes"
+
+func main() {
+	filename := os.Args[1]
+	cursor, err := newLineCursor(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cursor.Close()
+	cursor.Prev(10)
+	text, err := cursor.Read()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Print(text)
+	os.Exit(0)
+}
+
+type lineCursor struct {
+	file *os.File
+	byteOffset int64
+	lineOffset int64
+	endOffset  int64
+}
+
+func newLineCursor(filename string) (cur lineCursor, err error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return
+	}
+	initialOffset, err := file.Seek(0, os.SEEK_END)
+	if err != nil {
+		file.Close()
+		return
+	}
+	return lineCursor{file, initialOffset, 0, initialOffset}, nil
+}
+
+func (cur *lineCursor) Close() {
+	cur.file.Close()
+}
+
+func (cur *lineCursor) Prev(n int64) (err error){
+	step := int64(n * -64)
+	targetOffset := cur.lineOffset - n - 1
+	for cur.lineOffset > targetOffset && cur.byteOffset > 0 {
+		newOffset, err := cur.file.Seek(cur.byteOffset + step, os.SEEK_SET)
+		if err != nil {
+			return err
+		}
+		chunk := make([]byte, cur.byteOffset - newOffset)
+		_, err = cur.file.Read(chunk)
+		if err != nil {
+			return err
+		}
+		for cur.lineOffset > targetOffset {
+			newlineIndex := bytes.LastIndex(chunk, []byte("\n"))
+			if newlineIndex == -1 {
+				break
+			}
+			cur.byteOffset = newOffset + int64(newlineIndex) + 1
+			cur.lineOffset--
+			chunk = chunk[:newlineIndex-1]
+		}
+	}
+	return
+}
+
+func (cur *lineCursor) Size() (int64) {
+	return cur.endOffset - cur.byteOffset
+}
+
+func (cur *lineCursor) Read() (text string, err error) {
+	data := make([]byte, cur.endOffset - cur.byteOffset)
+	size, err := cur.file.Read(data)
+	if err != nil {
+		return
+	}
+	return string(data[:size]), nil
+}
